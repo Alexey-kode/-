2894c8432dc71588fe450e8cac7d11bb86fcd795
Alexey-kode <aleks8644@mail.ru>
2026-05-29 15:44:05 +0300

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -1,30 +1,30 @@
-#ifndef GRAPH_H
+#ifndef GRAPH_H // проверяем, не был ли уже подключён этот заголовочный файл
 #define GRAPH_H
 
-#include <string>
-#include <vector>
-#include <map>
+#include <string> // подключаем библиотеку для работы со строками
+#include <vector> // подключаем библиотеку для работы с динамическими массивами
+#include <map> // подключаем библиотеку для хранения данных в виде ключ-значение
 
-using namespace std;
+using namespace std; // подключаем стандартное пространство имён
 
-class Graph {
-private:
-    map<string, vector<string>> adjacencyList;
+class Graph { // объявляем класс Graph для работы с графом
+private: // закрытая часть класса
+    map<string, vector<string>> adjacencyList; // список смежности: вершина и список её соседей
 
-public:
-    bool hasVertex(const string& vertex);
+public: // открытая часть класса
+    bool hasVertex(const string& vertex); // объявляем метод проверки существования вершины
 
-    void addVertex(const string& vertex);
+    void addVertex(const string& vertex); // объявляем метод добавления вершины
 
-    void addEdge(const string& firstVertex, const string& secondVertex);
+    void addEdge(const string& firstVertex, const string& secondVertex); // объявляем метод добавления ребра
 
-    void printGraph() const;
+    void printGraph() const; // объявляем метод вывода графа
 
-    vector<string> bfs(const string& startVertex);
+    vector<string> bfs(const string& startVertex); // объявляем метод обхода графа в ширину
 
-    vector<string> dfs(const string& startVertex);
+    vector<string> dfs(const string& startVertex); // объявляем метод обхода графа в глубину
 
-    vector<string> findPath(const string& startVertex, const string& endVertex);
-};
+    vector<string> findPath(const string& startVertex, const string& endVertex); // объявляем метод поиска пути
+}; // завершаем описание класса Graph
 
-#endif
+#endif // завершаем защиту от повторного подключения
